1b33e29e6455ce3c50ddb8fab23294052c0b1db5
다시가리(Re_Go) <144569961+jongmin0919@users.noreply.github.com>
2023-11-04 18:57:48 +0900

    [level 2] Title: H-Index, Time: 0.05 ms, Memory: 33.4 MB -BaekjoonHub

---
+++ 프로그래머스/2/42747. H－Index/H－Index.js
@@ -0,0 +1,12 @@
+function solution(citations) {
+    let answer = 0;
+    
+    citations.sort((a,b) => b-a);
+    
+    for(let i = 0 ; i < citations.length ; i++){
+        if(citations[i] >= i+1){
+            answer++
+        } 
+    }
+    return answer;
+}
